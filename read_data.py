4ecca1d7bd182f4be4c6dd162abd0cc69bcf309e
Lemonzino <gasp.lorenzo@gmail.com>
2020-01-11 15:39:21 +0100

    Started implementation
    V1- Risultati di merda

--- read_data.py
+++ read_data.py
@@ -4,10 +4,10 @@ from os.path import isfile, join
 import shutil
 import csv
 
-if __name__ == '__main__':
-    image_path = 'C:\\Users\\Lorenzo Stacchio\\Downloads\\WIDER_val\\images'
-    mat2 = scipy.io.loadmat('C:\\Users\\Lorenzo Stacchio\\Desktop\\Machine_learning_project\\dataset\\test_set'
-                            '\\wider_face_val.mat')
+
+def get_images_with_one_face_train_val(image_path, mat_path, output_path_val, output_path_train):
+    mat2 = scipy.io.loadmat(mat_path)
+
     # Print caratteristiche file mat
     # for k in mat2:
     #    print(k)
@@ -18,28 +18,67 @@ if __name__ == '__main__':
     # Noi siamo interessati solo alle feature bbx, che si
     onlyDir = [f for f in listdir(image_path) if not isfile(join(image_path, f))]
     list_all = zip(mat2["face_bbx_list"], mat2["file_list"], onlyDir)
-    #filenames_single_photos = np.array([])
-    #all_labels_nparray = np.array([])
+
+    #list_training = list_all[:(int((len(onlyDir) - 1) * 0.8))]
+    list_training = [
+        [zip(mat2["face_bbx_list"][:(int((len(onlyDir) - 1) * 0.8))], mat2["file_list"][:(int((len(onlyDir) - 1) * 0.8))], onlyDir[:(int((len(onlyDir) - 1) * 0.8))]), output_path_train],
+        [zip(mat2["face_bbx_list"][(int((len(onlyDir) - 1) * 0.8)):len(onlyDir)-1], mat2["file_list"][(int((len(onlyDir) - 1) * 0.8)):len(onlyDir)-1], onlyDir[(int((len(onlyDir) - 1) * 0.8)):len(onlyDir)-1]), output_path_val]
+    ]
+
+    for zip_training in list_training:
+        # filenames_single_photos = np.array([])
+        # all_labels_nparray = np.array([])
+        file_description = []
+        for bbx, f_l, dir_name in zip_training[0]:  # bbx is nested, f_l is directly connected to filenames of the folder
+            pair_photo_filename = zip(bbx[0], f_l[0])
+            for bbx_single_photo, single_photo_filename in pair_photo_filename:
+                if len(bbx_single_photo[0]) == 1:
+                    # print(bbx_single_photo)
+                    file_description.append((str(single_photo_filename[0][0]) + ".jpg"
+                                             , list(list(bbx_single_photo[0])[0])))
+                    shutil.copy(image_path + "\\" + dir_name + "\\" + str(single_photo_filename[0][0]) + ".jpg",
+                                zip_training[1])
+        print(len(file_description))
+        with open(zip_training[1] + "\\output.csv", "w", newline='') as f:
+            csv_out = csv.writer(f)
+            csv_out.writerow(['filename', 'labels'])
+            for row in file_description:
+                csv_out.writerow(row)
+
+
+def get_images_with_one_face_test(image_path, mat_path, output_path):
+    mat2 = scipy.io.loadmat(mat_path)
+
+    # Print caratteristiche file mat
+    # for k in mat2:
+    #    print(k)
+    # Con la chiave stringa si accede al gruppo di cartelle che contengono quella proprietà: Es. bbx_list Con la
+    # seconda chiave si accede alla cartella desiderata, ricorda che windows ordina gli indici con 0,1,10,11...19,
+    # 20 Con il terzo indice si accede a tutte le caratteristiche effettive delle foto contenute nella cartella Con
+    # il quarto indice alle feature della singola foto
+    # Noi siamo interessati solo alle feature bbx, che si
+    onlyDir = [f for f in listdir(image_path) if not isfile(join(image_path, f))]
+    list_all = zip(mat2["face_bbx_list"], mat2["file_list"], onlyDir)
+    # filenames_single_photos = np.array([])
+    # all_labels_nparray = np.array([])
     file_description = []
     for bbx, f_l, dir_name in list_all:  # bbx is nested, f_l is directly connected to filenames of the folder
         pair_photo_filename = zip(bbx[0], f_l[0])
         for bbx_single_photo, single_photo_filename in pair_photo_filename:
             if len(bbx_single_photo[0]) == 1:
-                #print(bbx_single_photo)
-                file_description.append((image_path + "\\" + dir_name + "\\" + str(single_photo_filename[0][0]) + ".jpg"
-                                         , bbx_single_photo[0]))
-                # print(image_path + "\\" + dir_name + "\\" + str(single_photo_filename[0][0]) + ".jpg")
+                # print(bbx_single_photo)
+                file_description.append((str(single_photo_filename[0][0]) + ".jpg"
+                                         , list(list(bbx_single_photo[0])[0])))
+                shutil.copy(image_path + "\\" + dir_name + "\\" + str(single_photo_filename[0][0]) + ".jpg", output_path)
     print(len(file_description))
-    #print(file_description)
-    #with open("C:\\Users\\Lorenzo Stacchio\\Desktop\\Machine_learning_project\\dataset\\train_set\\output.csv", "w", newline='') as f:
-    #    csv_out = csv.writer(f)
-    #    csv_out.writerow(['filename', 'labels'])
-    #    for row in file_description:
-    #        csv_out.writerow(row)
-    # Copia e incolla tutte le foto su un'altra directory a mia scelta
-    # Copy all the files to another directory
-    if True: #made to execute or no execute this code
-        for file_name,values in file_description:
-            shutil.copy(file_name, 'C:\\Users\\Lorenzo Stacchio\\Desktop\\Machine_learning_project\\dataset\\test_set\\images')
+    with open(output_path + "\\output.csv", "w", newline='') as f:
+        csv_out = csv.writer(f)
+        csv_out.writerow(['filename', 'labels'])
+        for row in file_description:
+            csv_out.writerow(row)
 
-    # Write file description
+
+if __name__ == '__main__':
+    get_images_with_one_face_train_val(".\\dataset\\WIDER_train\\images", ".\\dataset\\wider_face_split\\wider_face_train.mat",
+                                       ".\\dataset\\val", ".\\dataset\\train")
+    get_images_with_one_face_test(".\\dataset\\WIDER_val\\images", ".\\dataset\\wider_face_split\\wider_face_val.mat", ".\\dataset\\test")
--- resize_data_final_step.py
+++ resize_data_final_step.py
@@ -4,10 +4,16 @@ import ast
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-image_paths_and_description_file = [("//media//Disco_Secondario//datasets//face_segmentation//train_set//resized",
-                                     "//media//Disco_Secondario//datasets//face_segmentation//train_set//output.csv"),
-                                    ("//media//Disco_Secondario//datasets//face_segmentation//test_set//resized",
-                                     "//media//Disco_Secondario//datasets//face_segmentation//test_set//output.csv")]
+image_paths_and_description_file = [(".\\dataset\\train",
+                                     ".\\dataset\\train\\output.csv",
+                                     ".\\dataset\\train\\output_final.csv"),
+                                    (".\\dataset\\test",
+                                     ".\\dataset\\test\\output.csv",
+                                     ".\\dataset\\test\\output_final.csv"),
+                                    (".\\dataset\\val",
+                                     ".\\dataset\\val\\output.csv",
+                                     ".\\dataset\\val\\output_final.csv")
+                                    ]
 
 WIDTH_SCALE, HEIGHT_SCALE = 300, 300
 
@@ -24,6 +30,7 @@ def reduce_image_size_and_return_reduced_labels(image_path, labels, new_width, n
     # label = image_path.loc[image_path["filename"] == "0_Parade_marchingband_1_356.jpg"]["labels"]
     label_fixed = ast.literal_eval(labels)  # to threat the string as tuple
     label_x, label_y, label_width, label_height = label_fixed[0], label_fixed[1], label_fixed[2], label_fixed[3]
+
     # print(label_x)
     # print(label_y)
     # print(label_width)
@@ -32,23 +39,30 @@ def reduce_image_size_and_return_reduced_labels(image_path, labels, new_width, n
     new_label_x, new_label_y, new_label_width, new_label_height = round(label_x / scaling_factor_width), round(
         label_y / scaling_factor_height), round(label_width / scaling_factor_width), round(
         label_height / scaling_factor_height)
-    if("0_Parade_marchingband_1_849.jpg" in image_path):
-        print([new_label_x, new_label_y, new_label_width, new_label_height])
-        im = im.resize((new_width, new_height))
-        draw = ImageDraw.Draw(im)
-        draw.rectangle(((new_label_x, new_label_y), (new_label_x + new_label_width, new_label_y + new_label_height)),
-            outline="Red")
-        im.save("test.jpg")
-        img = mpimg.imread('test.jpg')
-        imgplot = plt.imshow(img)
-        plt.show()
+
+    im = im.resize((new_width, new_height))
+
+    #draw = ImageDraw.Draw(im)
+    #draw.rectangle(((new_label_x, new_label_y), (new_label_x + new_label_width, new_label_y + new_label_height)),
+    #    outline="Red")
+    #im.save("test.jpg")
+    #img = mpimg.imread('test.jpg')
+    #imgplot = plt.imshow(img)
+    #plt.show()
+
+
     return im, [new_label_x, new_label_y, new_label_width, new_label_height]
 
 
 if __name__ == '__main__':
     for el in image_paths_and_description_file:
         print("Processing " + el[0])
+        data_frame_finale = pd.DataFrame(columns=["filename", "labels", "x_start", "y_start", "width", "height"])
         data_frame_description_step2 = pd.read_csv(el[1])
+        data_frame_description_step2["x_start"] = 0
+        data_frame_description_step2["y_start"] = 0
+        data_frame_description_step2["width"] = 0
+        data_frame_description_step2["height"] = 0
         for index, row in data_frame_description_step2.iterrows():
             if index % 100 == 0:
                 print("Processed " + str(index) + " images")
@@ -56,7 +70,11 @@ if __name__ == '__main__':
             img_reduced, new_labels = reduce_image_size_and_return_reduced_labels(image_path, row["labels"],
                                                                                   WIDTH_SCALE, HEIGHT_SCALE)
             img_reduced.save(image_path)  # this is a pillow image
-            row["labels"] = new_labels
-            if(row["filename"]=="0_Parade_marchingband_1_849.jpg"):
-                print(new_labels)
-        data_frame_description_step2.to_csv(el[1])
+            row["x_start"] = new_labels[0]
+            row["y_start"] = new_labels[1]
+            row["width"] = new_labels[2]
+            row["height"] = new_labels[3]
+            data_frame_finale = data_frame_finale.append(row, ignore_index=False)
+
+        del data_frame_finale["labels"]
+        data_frame_finale.to_csv(el[2])
