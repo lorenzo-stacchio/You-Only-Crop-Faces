b65e92a555712b9e4ff0a97720679c05ad973a54
Lorenzo <lorenzo.stacchio.dev@gmail.com>
2020-03-13 23:21:23 +0100

    Final parallel single face detector

--- balance_dataset_bb_and_classifier.py
+++ balance_dataset_bb_and_classifier.py
@@ -14,8 +14,8 @@ root_dat = "./dataset"
 bbox_path = "/data01/ML/dataset/FACE_CLASSIFIER/CelebA2/CelebA/Anno/list_bbox_celeba.csv"
 bb_new_file_name = "./dataset/parsed_list_bbox_celeba.csv"
 
-face_image_path = "/data01/ML/dataset/FACE_CLASSIFIER/CelebA2/CelebA/Img/img_celeba/img_celeba/img_celeba"
-objects_image_path = "/data01/ML/dataset/FACE_CLASSIFIER/256_ObjectCategories"
+face_image_path1 = "/data01/ML/dataset/FACE_CLASSIFIER/CelebA2/CelebA/Img/img_celeba/img_celeba/img_celeba"
+objects_image_path = "/data01/ML/dataset/FACE_CLASSIFIER/256_ObjectCategories_modified"
 
 output_final_scaled_list = ["./dataset/NUOVO/train/output_final.csv","./dataset/NUOVO/val/output_final.csv","./dataset/NUOVO/test/output_final.csv"]
 
@@ -31,7 +31,7 @@ def prepare_bb_txt_file():
                 fw.writelines(tmp)
 
 
-def get_faces(balanced, face_image_path, bbox_csv_path):
+def get_faces(balanced, face_image_path,  bbox_csv_path):
     df = pd.read_csv(bbox_csv_path)
     df_temp = df.sample(n=balanced)
     count = 0
@@ -54,7 +54,7 @@ def get_faces(balanced, face_image_path, bbox_csv_path):
         x_2, y_2 = x_1+w_bb, y_1+h_bb
         #x_center, y_center = temp[1]["x_1"] + int(temp[1]["width"]/2),temp[1]["y_1"] + int(temp[1]["height"]/2)
         filenames.append(face_image_path + "/" + str(temp[1]["image_id"]))
-        classification.append(np.array([x_1,y_1, w_bb, h_bb]))
+        classification.append((1, x_1, y_1, w_bb, h_bb))
     return filenames, classification
 
 
@@ -67,7 +67,7 @@ def get_balanced_data_from_classes(objects_main_folder):
                  isfile(join(objects_main_folder + "/" + dir_, f))]
         for f in files:
             filenames.append(objects_main_folder + "/" + dir_ + "/" + f)
-            classification.append([0, 0, 0, 0])
+            classification.append((0, 0, 0, 0, 0))
     return filenames, classification
 
 
@@ -87,41 +87,39 @@ def get_min_between_celeba_and_objects(objects_main_folder, celeba_images_folder
 
 
 
-def write_csv(category, faces):
+def write_csv(category, non_faces, faces):
     with open(root + "/" + category + ".csv", mode='w', newline='') as file:
         file = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        file.writerow(['image_path','x','y','x_2','y_2'])
-        #for el in non_faces:
-        #    file.writerow([el[0], el[1][0], el[1][1], el[1][2], el[1][3]])
-            #file.writerow([el[0], el[1][0],el[1][1],el[1][2],el[1][3],el[1][4]])
+        file.writerow(['image_path','face','x','y','x_2','y_2'])
+        for el in non_faces:
+            #print(el[1])
+            file.writerow([el[0], el[1][0],el[1][1],el[1][2],el[1][3],el[1][4]])
         for el in faces:
-            #file.writerow([el[0], el[1][0],el[1][1],el[1][2],el[1][3],el[1][4]])
-            file.writerow([el[0], el[1][0], el[1][1], el[1][2], el[1][3]])
+            file.writerow([el[0], el[1][0],el[1][1],el[1][2],el[1][3],el[1][4]])
 
 
 
 if __name__ == '__main__':
     #prepare_bb_txt_file()
-    min = get_min_between_celeba_and_objects(objects_image_path, face_image_path)
-    # non_faces = get_balanced_data_from_classes(objects_image_path)
-    #
-    # print(len(non_faces[0]))
-    # # print(non_faces[1])
-    # non_faces_x_train, non_faces_x_test, non_faces_y_train, non_faces_y_test = train_test_split(non_faces[0],
-    #                                                                                             non_faces[1],
-    #                                                                                             test_size=0.15,
-    #                                                                                             shuffle=True)
-    # non_faces_x_train, non_faces_x_val, non_faces_y_train, non_faces_y_val = train_test_split(non_faces_x_train,
-    #                                                                                           non_faces_y_train,
-    #                                                                                           test_size=0.15,
-    #                                                                                           shuffle=True)
-    # print(len(non_faces_x_train), len(non_faces_x_val), len(non_faces_x_test))
-    # # for el in zip(non_faces_x_train, non_faces_y_train):
-    # #    print(el)
+    min = get_min_between_celeba_and_objects(objects_image_path, face_image_path1)
+    non_faces = get_balanced_data_from_classes(objects_image_path)
+    print(len(non_faces[0]))
+    # print(non_faces[1])
+    non_faces_x_train, non_faces_x_test, non_faces_y_train, non_faces_y_test = train_test_split(non_faces[0],
+                                                                                                non_faces[1],
+                                                                                                test_size=0.15,
+                                                                                                shuffle=True)
+    non_faces_x_train, non_faces_x_val, non_faces_y_train, non_faces_y_val = train_test_split(non_faces_x_train,
+                                                                                              non_faces_y_train,
+                                                                                              test_size=0.15,
+                                                                                              shuffle=True)
+    print(len(non_faces_x_train), len(non_faces_x_val), len(non_faces_x_test))
+    # for el in zip(non_faces_x_train, non_faces_y_train):
+    #    print(el)
 
     #faces = get_faces(len(non_faces[0]), face_image_path, bb_new_file_name)
     #202599
-    faces = get_faces(202599, face_image_path, bb_new_file_name)
+    faces = get_faces(min, face_image_path, bb_new_file_name)
     print(len(faces[0]))
     # print(faces[1])
     faces_x_train, faces_x_test, faces_y_train, faces_y_test = train_test_split(faces[0], faces[1], test_size=0.15,
@@ -130,19 +128,11 @@ if __name__ == '__main__':
                                                                               test_size=0.15, shuffle=True)
     print(len(faces_x_train), len(faces_x_val), len(faces_x_test))
     # Define true dataset
-    #non_faces_train, faces_train = zip(non_faces_x_train, non_faces_y_train), zip(faces_x_train, faces_y_train)
-    #non_faces_val, faces_val = zip(non_faces_x_val, non_faces_y_val), zip(faces_x_val, faces_y_val)
-    #non_faces_test, faces_test = zip(non_faces_x_test, non_faces_y_test), zip(faces_x_test, faces_y_test)
+    non_faces_train, faces_train = zip(non_faces_x_train, non_faces_y_train), zip(faces_x_train, faces_y_train)
+    non_faces_val, faces_val = zip(non_faces_x_val, non_faces_y_val), zip(faces_x_val, faces_y_val)
+    non_faces_test, faces_test = zip(non_faces_x_test, non_faces_y_test), zip(faces_x_test, faces_y_test)
 
-    for tuple in [("train2", zip(faces_x_train,faces_y_train)), ("val2", zip(faces_x_val,faces_y_val)),
-                  ("test2", zip(faces_x_test,faces_y_test))]:
+    for tuple in [("train2", non_faces_train, faces_train), ("val2", non_faces_val, faces_val),
+                      ("test2", non_faces_test, faces_test)]:
         print("Writing:", tuple[0])
-        write_csv(tuple[0], tuple[1])
-
-    #for tuple in [("train2", non_faces_train, faces_train), ("val2", non_faces_val, faces_val),
-        #              ("test2", non_faces_test, faces_test)]:
-        #print("Writing:", tuple[0])
-        #write_csv(tuple[0], tuple[1], tuple[2])
-
-    # for el in faces:
-    #     file.writerow([el[0], el[1]])
+        write_csv(tuple[0], tuple[1], tuple[2])
